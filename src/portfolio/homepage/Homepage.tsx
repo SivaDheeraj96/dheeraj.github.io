5cf7322b9386e1e1d9c846e81518e34b8d761b37
SivaDheerajReddyObulamPersonal <obulam.dheeraj@gmail.com>
2022-10-21 04:20:14 -0400

    home page

--- src/portfolio/homepage/Homepage.tsx
+++ src/portfolio/homepage/Homepage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Home } from '../Home/Home';
 import { LeftRail } from './LeftRail';
 import styles from './styles/homepage.module.scss';
 
 export interface Section_t {
-  type : 'home' | "skills" | 'experience' | 'contact';
+  type : 'home' | "skills" | 'experience' | 'education' | 'contact';
 }
 export const Homepage: React.FC = () => {
   const [section, setSection] = React.useState<Section_t['type']>('home');
@@ -13,20 +14,24 @@ export const Homepage: React.FC = () => {
       <LeftRail setSection={setSection}></LeftRail>
       <div className={styles['body-content']} >
         <div className={styles['header-container']} >
-          <span className={styles['title']}> Welcome to My Website</span>
+          <span className={styles['title']}> {section.toUpperCase()}</span>
         </div>
         {section === 'home' &&
-          (<div className='home-container'>This is My Home Page</div>)
+          (<Home/>)
         }
         {section === 'skills' &&
-          (<div className='skills-container'>This is My Skills Page</div>)
+          (<div className='skills-container'>This is My Skills Page; still under construction </div>)
         }
         {section === 'experience' &&
-          (<div className='experience-container'>This is My Experience Page</div>)
+          (<div className='experience-container'>This is My Experience Page; still under construction</div>)
+        }
+        {section === 'education' &&
+          (<div className='education-container'>This is My Education Page; still under construction</div>)
         }
         {section === 'contact' &&
-          (<div className='contact-container'>This is My Contact Page</div>)
+          (<div className='contact-container'>This is My Contact Page; still under construction</div>)
         }
+        
       </div>
     </div>
   </>
