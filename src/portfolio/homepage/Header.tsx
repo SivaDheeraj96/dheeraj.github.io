a978b3247d32569e58d09636496b3a74b03d2e16
SivaDheerajReddyObulamPersonal <obulam.dheeraj@gmail.com>
2022-10-21 01:29:56 -0400

    Basic sections addition

---
+++ src/portfolio/homepage/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/header.module.scss';
+
+export const LeftRail = () => {
+  return (
+    <div className={styles["left-rail-container"]}>
+      <div className={styles["left-menu-item"]}>
+
+      </div>
+    </div>
+  );
+};
